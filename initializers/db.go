1ea4ff19154ec1b096dacdfe6fd5352cf9fa155d
wpcodevo <ziddah.edem@gmail.com>
2023-03-25 23:20:03 +0000

    crud api with fiber, mysql, and gorm

---
+++ initializers/db.go
@@ -0,0 +1,33 @@
+package initializers
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/wpcodevo/golang-fiber-mysql/models"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+
+func ConnectDB(config *Config) {
+	var err error
+	// dsn := fmt.Sprintf("user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", config.DBUserName, config.DBUserPassword, config.DBHost, config.DBPort, config.DBName)
+
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to the Database! \n", err.Error())
+		os.Exit(1)
+	}
+
+	DB.Logger = logger.Default.LogMode(logger.Info)
+
+	log.Println("Running Migrations")
+	DB.AutoMigrate(&models.Note{})
+
+	log.Println("🚀 Connected Successfully to the Database")
+}
---
+++ initializers/loadEnv.go
@@ -0,0 +1,31 @@
+package initializers
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	DBHost         string `mapstructure:"MYSQL_HOST"`
+	DBUserName     string `mapstructure:"MYSQL_USER"`
+	DBUserPassword string `mapstructure:"MYSQL_PASSWORD"`
+	DBName         string `mapstructure:"MYSQL_DATABASE"`
+	DBPort         string `mapstructure:"MYSQL_PORT"`
+
+	ClientOrigin string `mapstructure:"CLIENT_ORIGIN"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigType("env")
+	viper.SetConfigName("app")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
